26296b56f5a3e16f90d463ea318cc5a5033b3ce6
Vansh Sachdev <vksvs25@gmail.com>
2022-12-30 13:49:58 +0530

    Update

--- middleware.js
+++ middleware.js
@@ -1,3 +1,4 @@
+const ExpressError = require('./utils/ExpressError');
 module.exports.IsLoggedIn = (req, res, next) => {
     if (!req.session.user) {
         // req.flash('error', 'You must be signed in first')
